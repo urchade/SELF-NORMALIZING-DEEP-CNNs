7d76353b7f56293ec6c73425fddd7d3f26beff33
your_username <urchade.zaratiana@gmail.com>
2020-06-27 20:25:20 +0200

    yeeees

--- test.py
+++ test.py
@@ -3,6 +3,6 @@ import torch
 
 model = SNDCNNBlock(3, 3, 3)
 
-minibatch_images = torch.randn(size=(5, 55, 55, 3), dtype=torch.float32).reshape((5, 3, 55, 55))
+minibatch_images = torch.randn(size=(5, 3, 55, 55), dtype=torch.float32)
 
 x = model(minibatch_images)
